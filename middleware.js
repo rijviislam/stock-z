00a2039ecad55cecd34f38124736dc99f79558da
rijviislam <rijviislam54@gmail.com>
2025-06-16 23:45:56 +0600

    fix: fix the middleware

--- middleware.js
+++ middleware.js
@@ -1,18 +1,16 @@
-import { cookies } from "next/headers";
+
+
 import { NextResponse } from "next/server";
-
-export const middleware = async (req) => {
-const cookieStore = await cookies();
-const token =
-  cookieStore.get("next-auth.session-token") ||
-  cookieStore.get("__Secure-next-auth.session-token");
-
-
-
-  if (!token && req.nextUrl.pathname !== "/login") {
-    return NextResponse.redirect(new URL("/login", req.url));
+export const middleware = (request) => {
+  const token =cookieStore.get("next-auth.session-token") ||
+    cookieStore.get("__Secure-next-auth.session-token");
+  console.log(token)
+  if(!token) {
+    return NextResponse.redirect(new URL("/login", request.url))
   }
-
-
   return NextResponse.next();
+    
+};
+export const config = {
+  matcher: ["/about"],
 };
