e69de8d7b33959c572ab32e03f7f1ee60d4593ff
rijviislam <rijviislam54@gmail.com>
2025-06-21 17:19:05 +0600

    feat: added loading and error page

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,3 +1,7 @@
 export default function loading() {
-  return <div>LLLoading</div>;
+  return (
+    <div className="mt-32 flex items-center justify-center h-screen">
+      <span className="loader"></span>
+    </div>
+  );
 }
--- components/BookmarkGrid.jsx
+++ components/BookmarkGrid.jsx
@@ -34,44 +34,46 @@ export default function BookmarkGrid() {
   }, [email]);
 
   return (
-    <div className="flex items-center mt-28 flex-col">
+    <div className="flex items-center mt-28 flex-col min-h-screen">
       <h1 className="text-2xl  font-semibold text-gray-700 text-shadow-lg bg-transparent text-center lg:text-center">
         Bookmarks
       </h1>
-      {bookmarks.length === 0 ? (
-        <p>No bookmarks found</p>
+      {/* {bookmarks.length === 0 ? (
+        <loading />
       ) : (
-        <div className=" w-full flex items-center justify-center flex-wrap gap-5 mt-10">
-          {bookmarks.map((item) => (
-            <div
-              key={item._id}
-              className="max-w-[200px] h-[250px] px-2 py-3 bg-[#ecf6ff] shadow-md rounded-2xl group relative"
-            >
-              <div>
-                <Image
-                  src={item.image}
-                  alt="product image"
-                  quality={100}
-                  width={200}
-                  height={140}
-                  className="rounded-xl h-[140px] object-contain transition-transform transform group-hover:scale-110  duration-500"
-                />
-                <div className="px-3 ">
-                  <Link
-                    href={`/recommended/${item._id}`}
-                    className="text-base font-medium mt-4 hover:text-[#B35929]"
-                  >
-                    {item.title}
-                  </Link>
-                  <h3 className="text-[12px] font-normal mt-2">
-                    {item.subTitle}
-                  </h3>
-                </div>
+   
+      )} */}
+
+      <div className=" w-full flex items-center justify-center flex-wrap gap-5 mt-10">
+        {bookmarks.map((item) => (
+          <div
+            key={item._id}
+            className="max-w-[200px] h-[250px] px-2 py-3 bg-[#ecf6ff] shadow-md rounded-2xl group relative"
+          >
+            <div>
+              <Image
+                src={item.image}
+                alt="product image"
+                quality={100}
+                width={200}
+                height={140}
+                className="rounded-xl h-[140px] object-contain transition-transform transform group-hover:scale-110  duration-500"
+              />
+              <div className="px-3 ">
+                <Link
+                  href={`/recommended/${item._id}`}
+                  className="text-base font-medium mt-4 hover:text-[#B35929]"
+                >
+                  {item.title}
+                </Link>
+                <h3 className="text-[12px] font-normal mt-2">
+                  {item.subTitle}
+                </h3>
               </div>
             </div>
-          ))}
-        </div>
-      )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
